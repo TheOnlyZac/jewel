cf1c7c9bd35141530d2263d6bd9b312692268f7b
TheOnlyZac <zacharykrasnow@gmail.com>
2021-11-12 12:21:08 -0500

    Started entity list population

--- Jewel/Form1.Designer.cs
+++ Jewel/Form1.Designer.cs
@@ -38,17 +38,27 @@
             this.YPosLabel = new System.Windows.Forms.Label();
             this.ZPosTitle = new System.Windows.Forms.Label();
             this.ZPosLabel = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.FkxListbox = new System.Windows.Forms.ListBox();
+            this.FkxRefreshBtn = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.EntityNameLabel = new System.Windows.Forms.Label();
+            this.EntityListBox = new System.Windows.Forms.ListBox();
+            this.WarpToSelected = new System.Windows.Forms.Button();
             this.FooterPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ProcIdTitle
             // 
             this.ProcIdTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.ProcIdTitle.AutoSize = true;
-            this.ProcIdTitle.Location = new System.Drawing.Point(8, 4);
+            this.ProcIdTitle.Font = new System.Drawing.Font("Arial", 8.25F);
+            this.ProcIdTitle.Location = new System.Drawing.Point(8, 2);
             this.ProcIdTitle.Name = "ProcIdTitle";
-            this.ProcIdTitle.Size = new System.Drawing.Size(62, 13);
+            this.ProcIdTitle.Size = new System.Drawing.Size(62, 14);
             this.ProcIdTitle.TabIndex = 0;
             this.ProcIdTitle.Text = "Process ID:";
             // 
@@ -56,9 +66,10 @@
             // 
             this.ProcIdLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.ProcIdLabel.AutoSize = true;
-            this.ProcIdLabel.Location = new System.Drawing.Point(67, 4);
+            this.ProcIdLabel.Font = new System.Drawing.Font("Arial", 8.25F);
+            this.ProcIdLabel.Location = new System.Drawing.Point(67, 2);
             this.ProcIdLabel.Name = "ProcIdLabel";
-            this.ProcIdLabel.Size = new System.Drawing.Size(46, 13);
+            this.ProcIdLabel.Size = new System.Drawing.Size(44, 14);
             this.ProcIdLabel.TabIndex = 1;
             this.ProcIdLabel.Text = "$ProcID";
             // 
@@ -69,98 +80,182 @@
             // XPosTitle
             // 
             this.XPosTitle.AutoSize = true;
-            this.XPosTitle.Location = new System.Drawing.Point(155, 104);
+            this.XPosTitle.Font = new System.Drawing.Font("Arial", 10F);
+            this.XPosTitle.Location = new System.Drawing.Point(20, 53);
             this.XPosTitle.Name = "XPosTitle";
-            this.XPosTitle.Size = new System.Drawing.Size(34, 13);
+            this.XPosTitle.Size = new System.Drawing.Size(16, 16);
             this.XPosTitle.TabIndex = 3;
-            this.XPosTitle.Text = "Sly X:";
+            this.XPosTitle.Text = "X";
             // 
             // FooterPanel
             // 
             this.FooterPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.FooterPanel.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.FooterPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.FooterPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.FooterPanel.Controls.Add(this.ProcIdLabel);
             this.FooterPanel.Controls.Add(this.ProcIdTitle);
-            this.FooterPanel.Location = new System.Drawing.Point(-3, 274);
+            this.FooterPanel.Location = new System.Drawing.Point(-3, 276);
             this.FooterPanel.Name = "FooterPanel";
-            this.FooterPanel.Size = new System.Drawing.Size(426, 20);
+            this.FooterPanel.Size = new System.Drawing.Size(563, 20);
             this.FooterPanel.TabIndex = 4;
             // 
             // XPosLabel
             // 
             this.XPosLabel.AutoSize = true;
-            this.XPosLabel.Location = new System.Drawing.Point(195, 104);
+            this.XPosLabel.Font = new System.Drawing.Font("Arial", 10F);
+            this.XPosLabel.Location = new System.Drawing.Point(42, 53);
             this.XPosLabel.Name = "XPosLabel";
-            this.XPosLabel.Size = new System.Drawing.Size(35, 13);
+            this.XPosLabel.Size = new System.Drawing.Size(46, 16);
             this.XPosLabel.TabIndex = 5;
-            this.XPosLabel.Text = "label1";
+            this.XPosLabel.Text = "$xPos";
             // 
             // YPosTitle
             // 
             this.YPosTitle.AutoSize = true;
-            this.YPosTitle.Location = new System.Drawing.Point(155, 117);
+            this.YPosTitle.Font = new System.Drawing.Font("Arial", 10F);
+            this.YPosTitle.Location = new System.Drawing.Point(20, 69);
             this.YPosTitle.Name = "YPosTitle";
-            this.YPosTitle.Size = new System.Drawing.Size(34, 13);
+            this.YPosTitle.Size = new System.Drawing.Size(17, 16);
             this.YPosTitle.TabIndex = 3;
-            this.YPosTitle.Text = "Sly Y:";
+            this.YPosTitle.Text = "Y";
             // 
             // YPosLabel
             // 
             this.YPosLabel.AutoSize = true;
-            this.YPosLabel.Location = new System.Drawing.Point(195, 117);
+            this.YPosLabel.Font = new System.Drawing.Font("Arial", 10F);
+            this.YPosLabel.Location = new System.Drawing.Point(42, 69);
             this.YPosLabel.Name = "YPosLabel";
-            this.YPosLabel.Size = new System.Drawing.Size(35, 13);
+            this.YPosLabel.Size = new System.Drawing.Size(47, 16);
             this.YPosLabel.TabIndex = 5;
-            this.YPosLabel.Text = "label1";
+            this.YPosLabel.Text = "$yPos";
             // 
             // ZPosTitle
             // 
             this.ZPosTitle.AutoSize = true;
-            this.ZPosTitle.Location = new System.Drawing.Point(155, 130);
+            this.ZPosTitle.Font = new System.Drawing.Font("Arial", 10F);
+            this.ZPosTitle.Location = new System.Drawing.Point(20, 85);
             this.ZPosTitle.Name = "ZPosTitle";
-            this.ZPosTitle.Size = new System.Drawing.Size(34, 13);
+            this.ZPosTitle.Size = new System.Drawing.Size(16, 16);
             this.ZPosTitle.TabIndex = 3;
-            this.ZPosTitle.Text = "Sly Z:";
+            this.ZPosTitle.Text = "Z";
             // 
             // ZPosLabel
             // 
             this.ZPosLabel.AutoSize = true;
-            this.ZPosLabel.Location = new System.Drawing.Point(195, 130);
+            this.ZPosLabel.Font = new System.Drawing.Font("Arial", 10F);
+            this.ZPosLabel.Location = new System.Drawing.Point(42, 85);
             this.ZPosLabel.Name = "ZPosLabel";
-            this.ZPosLabel.Size = new System.Drawing.Size(35, 13);
+            this.ZPosLabel.Size = new System.Drawing.Size(46, 16);
             this.ZPosLabel.TabIndex = 5;
-            this.ZPosLabel.Text = "label1";
+            this.ZPosLabel.Text = "$zPos";
             // 
-            // listBox1
+            // FkxListbox
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(2, 2);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(125, 264);
-            this.listBox1.TabIndex = 6;
+            this.FkxListbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FkxListbox.FormattingEnabled = true;
+            this.FkxListbox.Location = new System.Drawing.Point(0, 25);
+            this.FkxListbox.Name = "FkxListbox";
+            this.FkxListbox.Size = new System.Drawing.Size(127, 251);
+            this.FkxListbox.Sorted = true;
+            this.FkxListbox.TabIndex = 6;
+            this.FkxListbox.SelectedIndexChanged += new System.EventHandler(this.FkxListbox_SelectedIndexChanged);
+            // 
+            // FkxRefreshBtn
+            // 
+            this.FkxRefreshBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FkxRefreshBtn.Font = new System.Drawing.Font("Arial", 8.25F);
+            this.FkxRefreshBtn.Location = new System.Drawing.Point(4, 2);
+            this.FkxRefreshBtn.Name = "FkxRefreshBtn";
+            this.FkxRefreshBtn.Size = new System.Drawing.Size(249, 23);
+            this.FkxRefreshBtn.TabIndex = 7;
+            this.FkxRefreshBtn.Text = "Refresh";
+            this.FkxRefreshBtn.UseVisualStyleBackColor = true;
+            this.FkxRefreshBtn.Click += new System.EventHandler(this.FkxRefreshBtn_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.splitContainer1.Panel1.Controls.Add(this.EntityListBox);
+            this.splitContainer1.Panel1.Controls.Add(this.FkxListbox);
+            this.splitContainer1.Panel1.Controls.Add(this.FkxRefreshBtn);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.splitContainer1.Panel2.Controls.Add(this.WarpToSelected);
+            this.splitContainer1.Panel2.Controls.Add(this.EntityNameLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.YPosTitle);
+            this.splitContainer1.Panel2.Controls.Add(this.ZPosLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.XPosTitle);
+            this.splitContainer1.Panel2.Controls.Add(this.ZPosTitle);
+            this.splitContainer1.Panel2.Controls.Add(this.XPosLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.YPosLabel);
+            this.splitContainer1.Size = new System.Drawing.Size(560, 277);
+            this.splitContainer1.SplitterDistance = 259;
+            this.splitContainer1.TabIndex = 8;
+            // 
+            // EntityNameLabel
+            // 
+            this.EntityNameLabel.AutoSize = true;
+            this.EntityNameLabel.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.EntityNameLabel.Location = new System.Drawing.Point(14, 15);
+            this.EntityNameLabel.Name = "EntityNameLabel";
+            this.EntityNameLabel.Size = new System.Drawing.Size(119, 23);
+            this.EntityNameLabel.TabIndex = 6;
+            this.EntityNameLabel.Text = "$entityName";
+            // 
+            // EntityListBox
+            // 
+            this.EntityListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.EntityListBox.FormattingEnabled = true;
+            this.EntityListBox.Location = new System.Drawing.Point(125, 25);
+            this.EntityListBox.Name = "EntityListBox";
+            this.EntityListBox.Size = new System.Drawing.Size(133, 251);
+            this.EntityListBox.Sorted = true;
+            this.EntityListBox.TabIndex = 8;
+            this.EntityListBox.SelectedIndexChanged += new System.EventHandler(this.EntityListBox_SelectedIndexChanged);
+            // 
+            // WarpToSelected
+            // 
+            this.WarpToSelected.Location = new System.Drawing.Point(18, 104);
+            this.WarpToSelected.Name = "WarpToSelected";
+            this.WarpToSelected.Size = new System.Drawing.Size(75, 23);
+            this.WarpToSelected.TabIndex = 7;
+            this.WarpToSelected.Text = "Warp To";
+            this.WarpToSelected.UseVisualStyleBackColor = true;
+            this.WarpToSelected.Click += new System.EventHandler(this.WarpToSelected_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(421, 294);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.ZPosLabel);
-            this.Controls.Add(this.ZPosTitle);
-            this.Controls.Add(this.YPosLabel);
-            this.Controls.Add(this.YPosTitle);
-            this.Controls.Add(this.XPosLabel);
-            this.Controls.Add(this.XPosTitle);
+            this.ClientSize = new System.Drawing.Size(558, 296);
             this.Controls.Add(this.FooterPanel);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FooterPanel.ResumeLayout(false);
             this.FooterPanel.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -176,7 +271,12 @@
         private System.Windows.Forms.Label YPosLabel;
         private System.Windows.Forms.Label ZPosTitle;
         private System.Windows.Forms.Label ZPosLabel;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox FkxListbox;
+        private System.Windows.Forms.Button FkxRefreshBtn;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label EntityNameLabel;
+        private System.Windows.Forms.ListBox EntityListBox;
+        private System.Windows.Forms.Button WarpToSelected;
     }
 }
 
--- Jewel/Form1.cs
+++ Jewel/Form1.cs
@@ -13,18 +13,33 @@ namespace Jewel
 {
     public partial class Form1 : Form
     {
+        // initialize global vars
+        private Entity jt, selectedEntity;
+        private BindingList<FkxEntry> fkxEntries = new BindingList<FkxEntry>();
+        private BindingList<Entity> allEntities = new BindingList<Entity>();
+
+        // Constructor
         public Form1()
         {
             InitializeComponent();
             this.Load += new System.EventHandler(this.Form1_Load);
         }
-
+        
         public static Mem m = new Mem();
+
+        private static uint Rebase(uint adr)
+        {
+            if (adr < 0x20000000)
+            {
+                return adr + 0x20000000;
+            }
+            return adr;
+        }
 
         // Handle form loaded
         private void Form1_Load(object sender, EventArgs e)
-        {
-            Console.WriteLine("load");
+        {   
+            // Start background worker
             if (!backgroundWorker1.IsBusy)
             {
                 backgroundWorker1.RunWorkerAsync();
@@ -37,63 +52,191 @@ namespace Jewel
             Application.Exit();
         }
 
-        // Generate game classes
+        // Define game classes
         private class Entity
         {
+            private uint pointer;
+            private static uint oId = 0x18;
+            private static uint oTransform = 0x58;
+
             public Transform transform;
 
+            // Constructor
             public Entity(uint pEntity)
             {
-                this.transform = new Transform(m.ReadUInt((pEntity + 0x20000058).ToString("X")));
+                this.pointer = Rebase(pEntity);
+                this.transform = new Transform(m.ReadUInt((pointer + oTransform).ToString("X")));
+            }
+
+            public uint getPointer() { return pointer; }
+
+            public uint id
+            {
+                get { return m.ReadUInt(Rebase(pointer + oId).ToString("X")); }
             }
         }
 
         private class Transform
         {
+            private uint pointer;
+            private static uint oScaleX = 0x0;
+            private static uint oScaleY = 0x4;
+            private static uint oScaleZ = 0x8;
+            private static uint oPosition = 0x30;
+
+            public Vector scaleX;
+            public Vector scaleY;
+            public Vector scaleZ;
             public Vector position;
 
+            // Constructor
             public Transform(uint pTransform)
             {
-                this.position = new Vector(pTransform + 0x30);
+                this.pointer = Rebase(pTransform);
+                this.scaleX = new Vector((pointer + oScaleX));
+                this.scaleY = new Vector((pointer + oScaleY));
+                this.scaleZ = new Vector((pointer + oScaleZ));
+                this.position = new Vector((pointer + oPosition));
             }
+
+            public uint getPointer() { return pointer; }
         }
 
         private class Vector
         {
-            private uint pX, pY, pZ;
-
+            private uint pointer;
+            private static uint oX = 0x0;
+            private static uint oY = 0x4;
+            private static uint oZ = 0x8;
+            
+            // Constructor
             public Vector(uint pVector)
             {
-                Console.WriteLine(pVector.ToString("X"));
-                (this.pX, this.pY, this.pZ) =
-                    (pVector + 0x20000000, pVector + 0x20000004, pVector + 0x20000008);
-
-                Console.WriteLine(this.pX.ToString("X"));
+                this.pointer = Rebase(pVector);
             }
 
-            public float x
+            public uint getPointer() { return pointer; }
+
+            public float X
             {
-                get { return m.ReadFloat(pX.ToString("X")); }
-                set { m.WriteMemory(pX.ToString("X"), "float", value.ToString()); }
+                get { return m.ReadFloat((pointer + oX).ToString("X")); }
+                set { m.WriteMemory((pointer + oX).ToString("X"), "float", value.ToString()); }
             }
-            public float y
+            public float Y
             {
-                get { return m.ReadFloat(pY.ToString("X")); }
-                set { m.WriteMemory(pY.ToString("X"), "float", value.ToString()); }
+                get { return m.ReadFloat((pointer + oY).ToString("X")); }
+                set { m.WriteMemory((pointer + oY).ToString("X"), "float", value.ToString()); }
             }
-            public float z
+            public float Z
             {
-                get { return m.ReadFloat(pZ.ToString("X")); }
-                set { m.WriteMemory(pZ.ToString("X"), "float", value.ToString()); }
+                get { return m.ReadFloat((pointer + oZ).ToString("X")); }
+                set { m.WriteMemory((pointer + oZ).ToString("X"), "float", value.ToString()); }
+            }
+            
+            public override string ToString()
+            {
+                return (String.Format("({0}, {1}, {2})", X, Y, Z));
             }
         }
 
-        // initialize entity pointers
-        private Entity jt, bentley, murray;
-
-        private  void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        private class FkxEntry
         {
-            Console.WriteLine("bgworker started");
+            private static uint oPoolArray = 0x4;
+            private static uint oPoolSize = 0x8;
+            private static uint oEntityId = 0x10;
+            private static uint oName = 0x20;
+
+            public uint pointer;
+
+            // Constructor
+            public FkxEntry(uint pFkxEntry)
+            {
+                this.pointer = Rebase(pFkxEntry);
+            }
+
+            public uint poolArray
+            {
+                get { return m.ReadUInt((pointer + oPoolArray).ToString("X")); }
+            }
+
+            public uint poolSize
+            {
+                get { return m.ReadUInt((pointer + oPoolSize).ToString("X")); }
+            }
+
+            public uint entityId
+            {
+                get { return m.ReadUInt((pointer + oEntityId).ToString("X")); }
+            }
+
+            public string name
+            {
+                get { return m.ReadString((pointer + oName).ToString("X")); }
+            }
+        }
+
+        // Populate fkxentries list with all FKX entries in ram
+        public async void FindFkxEntries()
+        {
+            fkxEntries.Clear();
+            IEnumerable<long> AoBScanResults = await m.AoBScan("46 4B 24 58", false, true, false);
+
+            Console.WriteLine(AoBScanResults.Count());
+            // Add each found FKX entry to the fkxentries list
+            foreach (long result in AoBScanResults)
+            {
+                FkxEntry fkx = new FkxEntry((uint)(result) - 0x1c);
+                fkxEntries.Add(fkx);
+            }
+        }
+
+        // Handle change selected entity
+        private void FkxListbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = FkxListbox.SelectedIndex;
+            allEntities.Clear();
+            FkxEntry fkx = (FkxEntry)FkxListbox.SelectedItem;
+            for (uint i = 0; i < fkx.poolSize; i++)
+            {
+                uint entityPtr = Rebase(fkx.poolArray + (0x4 * i));
+                Console.WriteLine(m.ReadUInt(entityPtr.ToString("X")).ToString("X"));
+                allEntities.Add(new Entity(m.ReadUInt(entityPtr.ToString("X"))));
+            }
+        }
+
+        private void EntityListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedEntity = (Entity)EntityListBox.SelectedItem;
+            //Console.WriteLine(selectedEntity.getPointer().ToString("X"));
+        }
+
+        private void FkxRefreshBtn_Click(object sender, EventArgs e)
+        {
+            this.FindFkxEntries();
+            // Set FK$X entries listbox data source
+            FkxListbox.Invoke((MethodInvoker)delegate
+            {
+                FkxListbox.DataSource = fkxEntries;
+                FkxListbox.DisplayMember = "name";
+            });
+
+            // Set FK$X entries listbox data source
+            EntityListBox.Invoke((MethodInvoker)delegate
+            {
+                EntityListBox.DataSource = allEntities;
+                EntityListBox.DisplayMember = "id";
+            });
+        }
+
+        private void WarpToSelected_Click(object sender, EventArgs e)
+        {
+            jt.transform.position.X = selectedEntity.transform.position.Z;
+            jt.transform.position.Y = selectedEntity.transform.position.Y;
+            jt.transform.position.Z = selectedEntity.transform.position.Z;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
             int pID;
             bool isProcOpen = false;
 
@@ -140,45 +283,25 @@ namespace Jewel
                 {
                     jt = new Entity(m.ReadUInt("202e1e40"));
                 }
-                if (bentley == null)
+                
+                // write selected entity position to ui
+                if (selectedEntity != null)
                 {
-                    bentley = new Entity(m.ReadUInt("202dd5bc"));
-                }
+                    XPosLabel.Invoke((MethodInvoker)delegate
+                    {
+                        XPosLabel.Text = selectedEntity.transform.position.X.ToString();
+                    });
+                    YPosLabel.Invoke((MethodInvoker)delegate
+                    {
+                        YPosLabel.Text = selectedEntity.transform.position.Y.ToString();
+                    });
+                    ZPosLabel.Invoke((MethodInvoker)delegate
+                    {
+                        ZPosLabel.Text = selectedEntity.transform.position.Z.ToString();
+                    });
 
-                // read sly's position vector
-                XPosLabel.Invoke((MethodInvoker)delegate
-                {
-                    XPosLabel.Text = jt.transform.position.x.ToString();
-                });
-                YPosLabel.Invoke((MethodInvoker)delegate
-                {
-                    YPosLabel.Text = jt.transform.position.y.ToString();
-                });
-                ZPosLabel.Invoke((MethodInvoker)delegate
-                {
-                    ZPosLabel.Text = jt.transform.position.z.ToString();
-                });
+                }
             }
         }
-
-        /* public partial class Vector
-        {
-            public float x, y, z;
-
-            public Vector()
-            {
-                (this.x, this.y, this.z) = (0, 0, 0);
-            }
-
-            public Vector(float x, float y, float z)
-            {
-                (this.x, this.y, this.z) = (x, y, z);
-            }
-
-            public override string ToString()
-            {
-                return (String.Format("({0}, {1}, {2})", this.x, this.y, this.z));
-            }
-        } */
     }
 }
